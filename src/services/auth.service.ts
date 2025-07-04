9d45a9908912d3f32f03772ffc2b662385992cae
Wilhelm Flores <wiljoseflores@gmail.com>
2025-07-04 01:11:54 -0400

    feat: add toast and alerts

---
+++ src/services/auth.service.ts
@@ -0,0 +1,22 @@
+import Api from "@/services";
+
+export async function loginUser(userOrEmail: string, password: string) {
+  try {
+    const data = await Api.post<{token: string}>('/auth/login', {
+      userOrEmail,
+      password,
+    })
+    return data.token
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function validateToken() {
+  try {
+    const data = await Api.get<{token: string}>('/auth/validate-token')
+    return data.token
+  } catch (error) {
+    throw error;
+  }
+}
